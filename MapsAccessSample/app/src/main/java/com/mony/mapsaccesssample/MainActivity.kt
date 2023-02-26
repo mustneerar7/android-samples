dfa74391521285e674ddb6930cd557510f92f3ce
mustneerar7 <mustneerahmad@outlook.com>
2023-02-26 20:12:58 +0500

    maps access sample added

---
+++ MapsAccessSample/app/src/main/java/com/mony/mapsaccesssample/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.mony.mapsaccesssample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+/* Implementing OnMapReadyCallback interface in parent activity. */
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /* Access a view to display map. */
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    /* An override method to setup map properties. */
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+    }
+}
